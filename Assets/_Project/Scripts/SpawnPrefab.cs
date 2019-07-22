b527a9e71cb7571ebffa5a059da0b2d298183274
Johannes Deml <public@deml.io>
2019-07-22 13:29:40 +0200

    Add simple unity project

---
+++ Assets/_Project/Scripts/SpawnPrefab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPrefab : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab = null;
+    
+    void Start()
+    {
+        InstantiatePrefab();
+    }
+
+    private void InstantiatePrefab()
+    {
+        if (prefab != null)
+        {
+            Instantiate(prefab, transform);
+        }
+    }
+}
